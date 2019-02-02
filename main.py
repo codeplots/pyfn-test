59347fb38ada6ff07429c5ba5fe4fd2750af2c4f
Julian Vossen <julvo@yahoo.de>
2019-02-02 17:36:11 +0000

    Add fn with args

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def handle():
     print("Hello from handle")
     return "Returning from handle"
+
+def handle_with_args(arg1, arg2):
+    print("Hello from handle_with_args", arg1, arg2)
+    return arg1
